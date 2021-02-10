e3847a5d4fbc2d79ec2ed8c485707f9755bdea4a
ItsmeLee <arsmagica90@gmail.com>
2021-02-11 00:03:15 +0900

    Add watch file function
    Add watch file function in gulp.babel.js file.
    If the pug file will be edit, the web server automatically refresh site.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,8 @@
-import gulp from "gulp"
-import gulpPug from "gulp-pug"
+import gulp from "gulp";
+import gulpPug from "gulp-pug";
+import del from "del";          // build 파일 청소 모듈
+import gulpWebServer from "gulp-webserver"
+
 
 const routes = {
     pug:{
@@ -8,6 +11,20 @@ const routes = {
     }
 }
 
-export const pug = () => gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest))
+const pug = () => gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest))
 
-export const dev = gulp.series([pug])
+const clean = () => del(["build"])
+
+const webserver = () => gulp.src("build").pipe(gulpWebServer({port:5000, livereload : true}))
+
+const watch = () =>{
+    gulp.watch(routes.pug.src, pug);  
+} 
+
+const prepare = gulp.series([clean])
+
+const assets = gulp.series([pug])
+
+const postDev = gulp.series([webserver, watch])
+
+export const dev = gulp.series([prepare, assets, postDev])
