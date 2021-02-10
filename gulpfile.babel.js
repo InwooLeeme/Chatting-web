8ac983f835896d125c4cabf09c416140281e2d8d
ItsmeLee <arsmagica90@gmail.com>
2021-02-10 22:47:44 +0900

    Test run gulp task
    Did some test the gulp task actually run.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,13 @@
 import gulp from "gulp"
+import gulpPug from "gulp-pug"
 
-export const dev = () => console.log(`I will dev`);
+const routes = {
+    pug:{
+        src:"src/**/*.pug",
+        dest:"build"
+    }
+}
+
+export const pug = () => gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest))
+
+export const dev = gulp.series([pug])
